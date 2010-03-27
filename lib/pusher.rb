03958f706f2c9458bba970d839b9a00636eeb063
Ismael Celis <ismaelct@gmail.com>
2010-03-27 16:26:15 +0000

    Specs passing individually but weird stuff with Net::HTTP mocks

--- lib/pusher.rb
+++ lib/pusher.rb
@@ -3,7 +3,13 @@ require 'uri'
 require 'net/http'
 
 class Pusher
-
+  
+  class ArgumentError < ::ArgumentError
+    def message
+      'You must configure both Pusher.key and Pusher.secret in order to authenticate your Pusher app'
+    end
+  end
+  
   class << self
     attr_accessor :host, :port
     attr_writer :key, :secret
@@ -13,6 +19,7 @@ class Pusher
   self.port = 80
 
   def self.[](channel_id)
+    raise ArgumentError unless (@key && @secret)
     @channels ||= {}
     @channels[channel_id.to_s] ||= Channel.new(@key, channel_id)
   end
--- spec/pusher_spec.rb
+++ spec/pusher_spec.rb
@@ -1,7 +1,98 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Test" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Pusher do
+  
+  describe 'configuration' do
+    it 'should be preconfigured for api host' do
+      Pusher.host.should == 'api.pusherapp.com'
+    end
+    
+    it 'should be preconfigured for pi port 80' do
+      Pusher.port.should == 80
+    end
+  end
+  
+  describe 'unconfigured' do
+    describe 'with missing key and secret' do
+      it 'should raise exception' do
+        lambda {
+          Pusher['test-channel']
+        }.should raise_error(Pusher::ArgumentError)
+      end
+    end
+
+    describe 'with missing key' do
+      before {Pusher.secret = '1234567890'}
+      it 'should raise exception' do
+        lambda {
+          Pusher['test-channel']
+        }.should raise_error(Pusher::ArgumentError)
+      end
+    end
+
+    describe 'with missing secret' do
+      before {Pusher.key = '1234567890'}
+      it 'should raise exception' do
+        lambda {
+          Pusher['test-channel']
+        }.should raise_error(Pusher::ArgumentError)
+      end
+    end
+  end
+  
+  
+  describe 'configured' do
+    before do
+      Pusher.key    = '12345678900000001'
+      Pusher.secret = '12345678900000001'
+    end
+    
+    describe '.[]' do
+      before do
+        @channel = Pusher['test_channel']
+      end
+      
+      it 'should return a new channel' do
+        @channel.should be_kind_of(Pusher::Channel)
+      end
+    end
+    
+    describe 'Channel#trigger' do
+
+      before do
+        @http = mock('HTTP', :post => 'posting')
+        Net::HTTP.stub!(:new).and_return @http
+      end
+
+      it 'should configure HTTP library to talk to pusher API' do
+        Net::HTTP.should_receive(:new).with('api.pusherapp.com', 80).and_return @http
+        Pusher['test_channel'].trigger(
+          'new_event',
+          'Some data'
+        )
+      end
+      
+      describe 'POSTing to api.pusherapp.com' do
+        
+        it 'should POST JSON to pusher API' do
+          @http.should_receive(:post).with(
+            '/app/12345678900000001/channel/test_channel',
+            "{\"event\":\"new_event\",\"data\":{\"last_name\":\"App\",\"name\":\"Pusher\"},\"socket_id\":null}",
+            {'Content-Type'=> 'application/json'}
+          )
+          Pusher['test_channel'].trigger(
+            'new_event',
+            :name => 'Pusher',
+            :last_name => 'App'
+          )
+        end
+      end
+    end
+    
+  end
+  
+  after do
+    Pusher.key = nil
+    Pusher.secret = nil
   end
 end
