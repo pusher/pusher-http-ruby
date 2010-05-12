349c546569110d07d59c349627d85c12abbf493c
Ben Pickles and Martyn Loughran <bambinos+benpickles+mloughran@new-bamboo.co.uk>
2010-05-12 15:30:31 +0100

    Allow user-defined token when generating a request.

--- lib/pusher/request.rb
+++ lib/pusher/request.rb
@@ -2,7 +2,7 @@ module Pusher
   class Request
     attr_reader :body, :query
 
-    def initialize(uri, event_name, data, socket_id = nil)
+    def initialize(resource, event_name, data, socket_id, token = nil)
       params = {
         :name => event_name,
       }
@@ -21,8 +21,8 @@ module Pusher
       end
       params[:body_md5] = Digest::MD5.hexdigest(body)
 
-      request = Signature::Request.new('POST', uri.path, params)
-      auth_hash = request.sign(Pusher.authentication_token)
+      request = Signature::Request.new('POST', resource.path, params)
+      auth_hash = request.sign(token || Pusher.authentication_token)
 
       @query = params.merge(auth_hash)
     end
