384fb544a38979c35648f559c9ef09dd0eeed4bd
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-03-03 22:33:05 +0100

    remove some load path mashing, its not needed as we are using bundler

--- spec/channel_spec.rb
+++ spec/channel_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../spec_helper', __FILE__)
+require 'spec_helper'
 
 describe Pusher::Channel do
   before do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,13 @@
-require 'rubygems'
+begin
+  require 'bundler/setup'
+rescue LoadError
+  puts 'although not required, it is recommended that you use bundler when running the tests'
+end
 
 require 'rspec'
 require 'rspec/autorun'
 require 'em-http' # As of webmock 1.4.0, em-http must be loaded first
 require 'webmock/rspec'
-
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'pusher'
 require 'eventmachine'
