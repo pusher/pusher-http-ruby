cd12eb5dfbd05f749cf212a5fd8a488618c6e83b
Paweł Ledwoń <me@ludvik.pl>
2012-01-20 16:04:06 +0000

    Refactored Pusher client code to separate class, delegating old Pusher.* methods to the default client instance.

--- lib/pusher.rb
+++ lib/pusher.rb
@@ -19,90 +19,42 @@ module Pusher
   class HTTPError < Error; attr_accessor :original_error; end
 
   class << self
-    attr_accessor :scheme, :host, :port
-    attr_writer :logger
-    attr_accessor :app_id, :key, :secret
+    extend Forwardable
 
-    # @private
-    def logger
-      @logger ||= begin
-        log = Logger.new($stdout)
-        log.level = Logger::INFO
-        log
+    def_delegators :default_client, :scheme, :host, :port, :app_id, :key, :secret, :logger
+    def_delegators :default_client, :scheme, :host=, :port=, :app_id=, :key=, :secret=, :logger=
+
+    def_delegators :default_client, :authentication_token, :url
+    def_delegators :default_client, :encrypted=, :url=
+
+    # Return a channel by name
+    #
+    # @example
+    #   Pusher['my-channel']
+    # @return [Channel]
+    # @raise [ConfigurationError] unless key, secret and app_id have been
+    #   configured
+    def [](channel_name)
+      begin
+        default_client[channel_name]
+      rescue ConfigurationError
+        raise ConfigurationError, 'Missing configuration: please check that Pusher.key, Pusher.secret and Pusher.app_id are configured.'
       end
-    end
-
-    # @private
-    def authentication_token
-      Signature::Token.new(@key, @secret)
-    end
-
-    # @private Builds a connection url for Pusherapp
-    def url
-      URI::Generic.build({
-        :scheme => self.scheme,
-        :host => self.host,
-        :port => self.port,
-        :path => "/apps/#{self.app_id}"
-      })
-    end
-
-    # Configure Pusher connection by providing a url rather than specifying
-    # scheme, key, secret, and app_id separately.
-    #
-    # @example
-    #   Pusher.url = http://KEY:SECRET@api.pusherapp.com/apps/APP_ID
-    #
-    def url=(url)
-      uri = URI.parse(url)
-      self.app_id = uri.path.split('/').last
-      self.key    = uri.user
-      self.secret = uri.password
-      self.host   = uri.host
-      self.port   = uri.port
-    end
-
-    # Configure whether Pusher API calls should be made over SSL
-    # (default false)
-    #
-    # @example
-    #   Pusher.encrypted = true
-    #
-    def encrypted=(boolean)
-      Pusher.scheme = boolean ? 'https' : 'http'
-      # Configure port if it hasn't already been configured
-      Pusher.port ||= boolean ? 443 : 80
     end
 
     private
 
-    def configured?
-      host && scheme && key && secret && app_id
+    def default_client
+      @default_client ||= Pusher::Client.new
     end
   end
 
-  # Defaults
-  self.scheme = 'http'
-  self.host = 'api.pusherapp.com'
-
   if ENV['PUSHER_URL']
-    self.url = ENV['PUSHER_URL']
-  end
-
-  # Return a channel by name
-  #
-  # @example
-  #   Pusher['my-channel']
-  # @return [Channel]
-  # @raise [ConfigurationError] unless key, secret and app_id have been
-  #   configured
-  def self.[](channel_name)
-    raise ConfigurationError, 'Missing configuration: please check that Pusher.key, Pusher.secret and Pusher.app_id are configured.' unless configured?
-    @channels ||= {}
-    @channels[channel_name.to_s] ||= Channel.new(url, channel_name)
+    url = ENV['PUSHER_URL']
   end
 end
 
+require 'pusher/client'
 require 'pusher/channel'
 require 'pusher/request'
 require 'pusher/webhook'
---
+++ lib/pusher/client.rb
@@ -0,0 +1,87 @@
+module Pusher
+  class Client
+    attr_accessor :scheme, :host, :port, :app_id, :key, :secret
+    attr_writer :logger
+
+    # Initializes the client object.
+    def initialize(options = {})
+      options = {
+        scheme: 'http',
+        host: 'api.pusherapp.com',
+      }.merge(options)
+      @scheme, @host, @port, @app_id, @key, @secret, @logger = options.values_at([
+        :scheme, :host, :port, :app_id, :key, :secret, :logger
+      ])
+    end
+
+    # Returns the logger associated to the client
+    def logger
+      @logger ||= begin
+        log = Logger.new($stdout)
+        log.level = Logger::INFO
+        log
+      end
+    end
+
+    # @private Returns the authentication token for the client
+    def authentication_token
+      Signature::Token.new(@key, @secret)
+    end
+
+    # @private Builds a connection url for Pusherapp
+    def url
+      URI::Generic.build({
+        :scheme => @scheme,
+        :host => @host,
+        :port => @port,
+        :path => "/apps/#{@app_id}"
+      })
+    end
+
+    # Configure Pusher connection by providing a url rather than specifying
+    # scheme, key, secret, and app_id separately.
+    #
+    # @example
+    #   Pusher.url = http://KEY:SECRET@api.pusherapp.com/apps/APP_ID
+    #
+    def url=(url)
+      uri = URI.parse(url)
+      @app_id = uri.path.split('/').last
+      @key    = uri.user
+      @secret = uri.password
+      @host   = uri.host
+      @port   = uri.port
+    end
+
+    # Configure whether Pusher API calls should be made over SSL
+    # (default false)
+    #
+    # @example
+    #   Pusher.encrypted = true
+    #
+    def encrypted=(boolean)
+      @scheme = boolean ? 'https' : 'http'
+      # Configure port if it hasn't already been configured
+      @port ||= boolean ? 443 : 80
+    end
+
+    # Return a channel by name
+    #
+    # @example
+    #   Pusher['my-channel']
+    # @return [Channel]
+    # @raise [ConfigurationError] unless key, secret and app_id have been
+    #   configured
+    def [](channel_name)
+      raise ConfigurationError, 'Missing client configuration: please check that key, secret and app_id are configured.' unless configured?
+      @channels ||= {}
+      @channels[channel_name.to_s] ||= Channel.new(url, channel_name)
+    end
+
+    private
+
+    def configured?
+      host && scheme && key && secret && app_id
+    end
+  end
+end
